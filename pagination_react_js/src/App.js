7fc6ea6f8f8eabff3d08c0e10aa7942ed3079537
GITA <jgitanjali592@gmail.com>
2024-06-23 13:55:15 +0530

    this code for pagination

--- pagination_react_js/src/App.js
+++ pagination_react_js/src/App.js
@@ -1,37 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import {useEffect, useState} from "react";
 import "./App.css";
+import Pagination from "./components/Pagination";
 
-const App = () => {
-  const [products , setProducts] = useState([])
-  const [page, setpage] = useState(1)
+function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
+  const fetchProducts = async () => {
+    const res = await fetch(`https://dummyjson.com/products?limit=100`);
+    const data = await res.json();
 
-  const fetchProducts = async() =>{
- const res =  await fetch('https://dummyjson.com/products?limit=100')
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
 
- const data = await res.json();
- if(data && data.products)
- setProducts(data.products);
-};
-
-console.log(products)
-  
-  useEffect(()=>{
-   fetchProducts();
-  } , [])
-
-
-  const selectPageHandler = (selectedPage) =>{
-
-    if(selectedPage >= 1 && 
-      selectedPage <= products.length/10  && selectedPage != page
-    )
-    setpage(selectedPage);
-  }
-
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
-       <div>
+    <div>
       {products.length > 0 && (
         <div className="products">
           {products.slice(page * 10 - 10, page * 10).map((prod) => {
@@ -45,32 +34,11 @@ console.log(products)
         </div>
       )}
 
-      {
-        products.length  > 0 && (<div className='pagination'>
-          <span 
-          className={page > 1 ? "" : "pagination__disable"}
-          onClick={()=> selectPageHandler( page - 1)
-        }>◀️</span>
-          {[...Array(products.length /10)].map((_ , i ) => {
-            return (<span 
-              className={page === i+1? "pagination__selected" :""}
-              onClick={()=> selectPageHandler( i +1)
-        } key={i}>{i + 1}</span>
-          );
-          })}
-          <span onClick={()=> selectPageHandler( page + 1)
-        }
-        className={page < products.length /10 ? "" : "pagination__disable"}
-        > ▶️ </span>
-        </div>
-        )}
-          
+      {products.length > 0 && (
+        <Pagination products={products} page={page} setPage={setPage} />
+      )}
     </div>
-  )
-         
-       
+  );
 }
 
-export default App
-
-
+export default App;
