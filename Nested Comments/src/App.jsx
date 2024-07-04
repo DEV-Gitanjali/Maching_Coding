aab5842fc86f525aa33724da0273850a14b11da5
GITA <jgitanjali592@gmail.com>
2024-07-04 21:06:15 +0530

    this code for maching coding question

---
+++ Nested Comments/src/App.jsx
@@ -0,0 +1,34 @@
+// Frontend System Design Question on building a Scalable Nested Comments Component will be discussed in this lesson along with Requirement gathering, HLD, LLD and Optimisations required to build a component like this.
+
+// Prerequisites - 
+// Part 1 - https://www.youtube.com/watch?v=zeY1Vw1dWJk
+
+// Notes -
+// HLD Diagram - https://app.eraser.io/workspace/S1H0YT0juQnSRC9nB6T8?origin=share
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
